47a61aad71cef1c4cdb837b8f1dcdbcb8be9515c
Lukas <lukas.stolz9@outlook.de>
2021-04-24 15:46:49 +0200

    Fix / Updated subnodule

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 
+
 def main():
     run = True
 
@@ -41,7 +42,7 @@ def main():
                     gearsVar -= 1
 
         draw_window(gearsVar)
-    
+
     pygame.display.quit()
 
 
@@ -50,8 +51,8 @@ def draw_window(gearsVar):
 
     # Gear Widget
     x, y, w, h = WIDTH//2, HEIGHT//6, None, None
-    gear = GearDisplay(WINDOW, x, y, w, h, None, WHITE, 5, (HEIGHT//2))
-    gearSize = gear.draw(gearsVar)
+    gear = GearDisplay(WINDOW, x, y, w, h, None, WHITE, 8, 5, (HEIGHT//2))
+    gearSize = gear.draw()
 
     # # Throttle Widget
     # throttle = ThrottleWidget(WINDOW, 0, 0, WIDTH//4, HEIGHT, BLACK)
@@ -59,42 +60,46 @@ def draw_window(gearsVar):
 
     # LeftHeaderBox Widget
     x, y, w, h = 10, 10, WIDTH//3, HEIGHT//3
-    leftHeaderBox = HeaderBox(WINDOW, x, y, w, h, BLACK, WHITE, 2, 5, HEIGHT//12)
-    leftHeaderBox = leftHeaderBox.draw("298 KPH", ("L3", "P2"), ("12", "P2"))
+    leftHeaderBox = HeaderBox(
+        WINDOW, x, y, w, h, BLACK, WHITE, ("298 KPH", (
+            "L3", "P2"), ("12", "P2")), 2, 5, HEIGHT//12)
+    leftHeaderBox = leftHeaderBox.draw()
 
     # RightHeaderBox Widget
     x, y, w, h = WIDTH - 10 - WIDTH//3, 10, WIDTH//3, HEIGHT//3
-    rightHeaderBox = HeaderBox(WINDOW, x, y, w, h, BLACK, WHITE, 2, 5, HEIGHT//12)
-    rightHeaderBoxSize = rightHeaderBox.draw("14.235", ("100°C", "96°C"), ("94°C", "90°C"))
+    rightHeaderBox = HeaderBox(
+        WINDOW, x, y, w, h, BLACK, WHITE,
+        ("14.235", ("100°C", "96°C"), ("94°C", "90°C")), 2, 5, HEIGHT//12)
+    rightHeaderBoxSize = rightHeaderBox.draw()
 
     # Delta Widget
-    x, y, w, h = WIDTH//2, gearSize[3], WIDTH//9, WIDTH//9
-    delta = Label(WINDOW, x, y, w, h, None, GREEN, 50)
-    deltaSize = delta.draw("-0.002")
+    x, y, w, h = WIDTH//2, HEIGHT//2, WIDTH//9, WIDTH//9
+    delta = Label(WINDOW, x, y, w, h, None, GREEN, "-0.002", 50)
+    deltaSize = delta.draw()
 
     # Overtake Widget
-    x, y, w, h = WIDTH//2, deltaSize[1] + deltaSize[3], WIDTH//1.5, 40
-    overtake = Label(WINDOW, x, y, w, h, GREEN, WHITE, 30)
-    overtakeSize = overtake.drawWithBackground("Overtake", GREEN)
+    x, y, w, h = WIDTH//2, 4*HEIGHT//6, WIDTH//1.5, 40
+    overtake = Label(WINDOW, x, y, w, h, GREEN, WHITE, ("Overtake", GREEN), 30)
+    overtake.drawWithBackground()
 
     # ERS Widget
-    x, y, w, h = WIDTH//2, overtakeSize[1] + overtakeSize[3], WIDTH//1.3, 10
-    ers = ProgressBar(WINDOW, x, y, w, h, BLACK, YELLOW)
-    ersSize = ers.draw(50)
+    x, y, w, h = WIDTH//2, 5*HEIGHT//6, WIDTH//1.3, 10
+    ers = ProgressBar(WINDOW, x, y, w, h, BLACK, YELLOW, 50)
+    ersSize = ers.draw()
 
     # D Widget
-    x, y, w, h = WIDTH//3.39, ersSize[1] + ersSize[3] + HEIGHT//18, WIDTH//2.8, 10
-    d = ProgressBar(WINDOW, x, y, w, h, BLACK, WHITE)
-    dSize = d.draw(30)
+    x, y, w, h = WIDTH//3.39, 5.5*HEIGHT//6, WIDTH//2.8, 10
+    d = ProgressBar(WINDOW, x, y, w, h, BLACK, WHITE, 30)
+    dSize = d.draw()
 
     # H Widget
-    x, y, w, h = WIDTH - WIDTH//3.39, ersSize[1] + ersSize[3] + HEIGHT//18, WIDTH//2.8, 10
-    d = ProgressBar(WINDOW, x, y, w, h, BLACK, WHITE)
-    dSize = d.draw(60)
-
+    x, y, w, h = WIDTH - \
+        WIDTH//3.39, 5.5*HEIGHT//6, WIDTH//2.8, 10
+    d = ProgressBar(WINDOW, x, y, w, h, BLACK, WHITE, 60)
+    dSize = d.draw()
 
     pygame.display.update()
 
+
 if __name__ == "__main__":
     main()
-
